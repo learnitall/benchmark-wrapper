2e81470c71267e9542789895ab3311dadeb24b94
maxusmusti <meyceoz@redhat.com>
2021-06-10 01:10:20 -0400

    Framework (to add)

--- snafu/pbench_wrapper/trigger_pbench.py
+++ snafu/pbench_wrapper/trigger_pbench.py
@@ -14,12 +14,26 @@ class Trigger_pbench:
         self.samples = args.samples
         self.create_local = args.create_local
 
+    def _register_tools(self):
+        pass
+
     def run_benchmark(self):
         if not os.path.exists(self.tool_dict_path):
             logger.critical("Workload file %s not found" % self.workload)
             exit(1)
-        for i in range(1, self.iterations + 1):
-            for s in range(1, self.samples + 1):
-                logger.info(i + s)
 
+        self._register_tools() #Add logic to check if remotes are used with create-local (illegal)
+
+        #CREATE BENCHMARK RUN DIR HERE + other setup
+
+        #INSERT TM START CALL HERE (including whether or not to start other things)
+
+        for i in range(1, self.iterations + 1):
+            #HANDLE NEW ITERATIONS HERE
+            for s in range(1, self.samples + 1):
+                #HANDLE NEW SAMPLES HERE
+                logger.info(i + s)
+        #COLLECT TRANSIENT DATA HERE (SEND RESULTS)
+
+        #INSERT TM STOP CALL HERE
         
